09ea020a2e7c7190f6bf013bb4d8b278ce65a9d7
Dan Stocker <dan@kwaia.com>
2019-06-01 15:52:52 +0100

    Fixes createNoop export in flowcode

--- modules/flowcode/src/index.ts
+++ modules/flowcode/src/index.ts
@@ -1,11 +1,11 @@
 export {connect} from "./utils/connect";
 export {copy} from "./utils/copy";
 export {createNode} from "./utils/createNode";
+export {createNoop, Noop} from "./nodes/Noop";
 export {disconnect} from "./utils/disconnect";
 export {InPorts} from "./types/InPorts";
 export {InPort} from "./types/InPort";
 export {Node} from "./types/Node";
-export {Noop} from "./nodes/Noop";
 export {OutPorts} from "./types/OutPorts";
 export {OutPort} from "./types/OutPort";
 export {Outputs} from "./types/Outputs";
